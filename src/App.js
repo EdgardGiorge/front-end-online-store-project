7d2e7eac819341c330e78d4e693b0d5d4133bee9
Alexandre <lutosaalexandre@gmail.com>
2022-02-09 16:53:15 -0300

    Corrige conflitos de merge

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React from 'react';
-<<<<<<< HEAD
-=======
 import { Route, BrowserRouter } from 'react-router-dom';
->>>>>>> 110946b3b16afe7b0109e59f12f0e456d915d175
 import './App.css';
 import PrimeiraTela from './components/PrimeiraTela';
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="App" />
-=======
     <div className="App">
       <BrowserRouter>
         <Route exact path="/" component={ PrimeiraTela } />
       </BrowserRouter>
     </div>
->>>>>>> 110946b3b16afe7b0109e59f12f0e456d915d175
   );
 }
 
